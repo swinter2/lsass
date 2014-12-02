c2b066ba51dd8873b365500544ba20a457665b44
Sam Winter <sam.winter@silvertech.com>
2014-12-02 16:16:36 -0500

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		watch: {
+			sass: {
+				files: ['sass/**/*.{scss,sass}','sass/_partials/**/*.{scss,sass}'],
+				tasks: ['sass:dist']
+			},
+			livereload: {
+				files: ['*.html', '*.php', 'js/**/*.{js,json}', 'css/*.css','img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
+				options: {
+					livereload: true
+				}
+			}
+		},
+		sass: {
+			options: {
+				sourceMap: true,
+				outputStyle: 'compressed'
+			},
+			dist: {
+				files: {
+					'css/styles.css': 'sass/styles.scss'
+				}
+			}
+		}
+	});
+	grunt.registerTask('default', ['sass:dist', 'watch']);
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+};
